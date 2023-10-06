0e8726514b3ae7171ae32dd17372df97f9bbed57
up-tri <ktarou78@gmail.com>
2023-10-06 09:11:21 +0900

    update dependencies

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link
             rel='preconnect'
             href='https://fonts.gstatic.com'
-            crossOrigin='true'
+            crossOrigin='anonymous'
           />
           <link
             href='https://fonts.googleapis.com/css2?family=Inconsolata:wght@500&family=M+PLUS+1p:wght@300;500&family=Zen+Kaku+Gothic+New:wght@400;700&display=swap'
